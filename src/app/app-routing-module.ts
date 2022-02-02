0d6153fdee249e70d9f5dc6f57eb52142a4219ef
webersoncarneiro <webersoncoimbra2014@gmail.com>
2022-02-01 22:01:48 -0300

    adding forms

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,9 +1,13 @@
+import { DataFormComponent } from './data-form/data-form.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TemplateFormComponent } from './template-form/template-form.component';
 
-const routes: Routes = [{
-
-}];
+const routes: Routes = [
+  {path: 'templateForm', component: TemplateFormComponent},
+  {path: 'dataForm', component: DataFormComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'templateForm'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
